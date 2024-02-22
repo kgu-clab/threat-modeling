46c71fa378532487e607dbd3f1743f837b6cc47b
Jeong-Ag <kjungu1105@gmail.com>
2024-02-22 22:38:49 +0900

    feat: 페이지 설명 Section 생성

---
+++ src/components/common/Section/Section.tsx
@@ -0,0 +1,44 @@
+import classNames from "classnames";
+
+interface SectionProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Section = ({ className, children }: SectionProps) => {
+  return (
+    <div
+      className={classNames(
+        "flex flex-col rounded-lg border bg-white p-4",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+interface SectionHeaderProps {
+  title: string;
+  children?: React.ReactNode;
+}
+
+Section.Header = ({ title, children }: SectionHeaderProps) => {
+  return (
+    <div className="flex items-center justify-between">
+      <p className="text-xl font-bold text-black">{title}</p>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+interface SectionBodyProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+Section.Body = ({ className, children }: SectionBodyProps) => {
+  return <div className={classNames("mt-4", className)}>{children}</div>;
+};
+
+export default Section;
