475fa77b12f3f882578a9f12b9a2fd7191c201eb
Jeong-Ag <kjungu1105@gmail.com>
2024-02-23 18:07:59 +0900

    feat: 페이지 레이아웃 및 디자인 수정

--- src/router/AppLayout.tsx
+++ src/router/AppLayout.tsx
@@ -1,10 +1,14 @@
+import Footer from "@/components/common/Footer/Footer";
 import ScrollToTop from "@/components/common/ScrollToTop/ScrollToTop";
+import Navbar from "@/components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <ScrollToTop>
+      <Navbar />
       <Outlet />
+      <Footer />
     </ScrollToTop>
   );
 };
