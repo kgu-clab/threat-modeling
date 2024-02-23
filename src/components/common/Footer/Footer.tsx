18415e1c386f09b184aa0ac57eef0e09fb2aee9a
Jeong-Ag <kjungu1105@gmail.com>
2024-02-23 18:04:48 +0900

    feat: Footer 생성

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="p-8 bg-gray-200 footer">
+      <aside>
+        <p className="text-xl font-semibold">Threat Modeling</p>
+        <p>DS Lab</p>
+
+        <ul className="text-xs leading-loose">
+          <li>Developed By 김도훈, 김관식, 한관희, 김가을, 김정은</li>
+          <li className="font-semibold">© DS-Lab. All rights reserved.</li>
+        </ul>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
