f9ff44ef315c22ebfcd16dc94e3f537b94590fb3
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-06 19:04:57 +0900

    chore: set path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import path, { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -6,6 +6,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@api": resolve(__dirname, "./src/api"),
+      "@assets": resolve(__dirname, "./src/assets"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@constants": resolve(__dirname, "./src/constants"),
+      "@hooks": resolve(__dirname, "./src/hooks"),
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@router": resolve(__dirname, "./src/router"),
+      "@styles": resolve(__dirname, "./src/styles"),
+      "@type": resolve(__dirname, "./src/types"),
+      "@utils": resolve(__dirname, "./src/utils"),
       "@": path.resolve(__dirname, "./src"),
     },
   },
