572a76f2535fbbce2fa0bf990008ec4514aff3bc
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-07 14:33:20 +0900

    remove: 불필요한 컴포넌트 삭제

--- src/components/common/Content/Content.tsx
+++
@@ -1,9 +0,0 @@
-interface ContentProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const Content = ({ children }: ContentProps) => {
-  return (
-    <div className="items-center w-full space-y-4 rounded-lg">{children}</div>
-  );
-};
-
-export default Content;
