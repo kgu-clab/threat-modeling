d3d908e3c8e6ea2d7b404f3c7cc54f6b133ae449
limehee <noop103@naver.com>
2024-03-12 19:35:58 +0900

    refactor(Optimize): 서버 요청에 대한 처리 최적화를 위한 Cve, Defend 테이블 인덱싱 추가

--- server/src/main/java/kr/re/dslab/threatmodeling/type/entity/Cve.java
+++ server/src/main/java/kr/re/dslab/threatmodeling/type/entity/Cve.java
@@ -2,6 +2,8 @@ package kr.re.dslab.threatmodeling.type.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +16,13 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "cve", indexes = {
+        @Index(name = "cve_cvss_idx", columnList = "cvss"),
+        @Index(name = "cve_primary_impact_idx", columnList = "primaryImpact"),
+        @Index(name = "cve_secondary_impact_idx", columnList = "secondaryImpact"),
+        @Index(name = "cve_exploitation_technique_idx", columnList = "exploitationTechnique"),
+        @Index(name = "cve_uncategorized_idx", columnList = "uncategorized")
+})
 public class Cve {
 
     @Id
