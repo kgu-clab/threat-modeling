4cb1b78284c9570a6d2a72f50f919b0ae26eac5b
limehee <noop103@naver.com>
2024-03-12 22:35:56 +0900

    refactor(Optimize): Attack과 관련한 Mitigation 및 Defend 정보를 QueryDSL을 이용해 가져오도록 변경

--- server/src/main/java/kr/re/dslab/threatmodeling/repository/MitigationRepository.java
+++ server/src/main/java/kr/re/dslab/threatmodeling/repository/MitigationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MitigationRepository extends JpaRepository<Mitigation, String> {
+public interface MitigationRepository extends JpaRepository<Mitigation, String>, MitigationRepositoryCustom {
 
 }
