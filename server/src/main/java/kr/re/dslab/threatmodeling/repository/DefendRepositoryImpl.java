eaf4a4bf0b0bef0a76be25088c71ac494a127a28
limehee <noop103@naver.com>
2024-03-12 22:35:56 +0900

    refactor(Optimize): Attack과 관련한 Mitigation 및 Defend 정보를 QueryDSL을 이용해 가져오도록 변경

---
+++ server/src/main/java/kr/re/dslab/threatmodeling/repository/DefendRepositoryImpl.java
@@ -0,0 +1,31 @@
+package kr.re.dslab.threatmodeling.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import kr.re.dslab.threatmodeling.type.dto.DefendResponseDto;
+import kr.re.dslab.threatmodeling.type.entity.QDefend;
+
+import java.util.List;
+
+public class DefendRepositoryImpl implements DefendRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public DefendRepositoryImpl(EntityManager entityManager) {
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    @Override
+    public List<DefendResponseDto> findDefendsByMitigationId(String mitigationId) {
+        QDefend defend = QDefend.defend;
+
+        return queryFactory.select(Projections.constructor(DefendResponseDto.class,
+                        defend.defendId,
+                        defend.defendName))
+                .from(defend)
+                .where(defend.mitigation.mitigationId.eq(mitigationId))
+                .fetch();
+    }
+
+}
--- server/src/main/java/kr/re/dslab/threatmodeling/type/dto/DefendResponseDto.java
+++ server/src/main/java/kr/re/dslab/threatmodeling/type/dto/DefendResponseDto.java
@@ -1,13 +1,10 @@
 package kr.re.dslab.threatmodeling.type.dto;
 
-import kr.re.dslab.threatmodeling.type.entity.Defend;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -19,18 +16,5 @@ public class DefendResponseDto {
     private String defendId;
 
     private String defendName;
-
-    public static List<DefendResponseDto> of(List<Defend> defends) {
-        return defends.stream()
-                .map(DefendResponseDto::of)
-                .toList();
-    }
-
-    public static DefendResponseDto of(Defend defend) {
-        return DefendResponseDto.builder()
-                .defendId(defend.getDefendId())
-                .defendName(defend.getDefendName())
-                .build();
-    }
 
 }
