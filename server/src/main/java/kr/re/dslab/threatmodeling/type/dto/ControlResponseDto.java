a2ece6f7d1ca347bd164f1038d004608e26e9f86
limehee <noop103@naver.com>
2024-03-12 22:12:00 +0900

    refactor(Optimize): Attack과 관련한 Control 정보를 QueryDSL을 이용해 가져오도록 변경

--- server/src/main/java/kr/re/dslab/threatmodeling/type/dto/ControlResponseDto.java
+++ server/src/main/java/kr/re/dslab/threatmodeling/type/dto/ControlResponseDto.java
@@ -1,30 +1,16 @@
 package kr.re.dslab.threatmodeling.type.dto;
 
-import kr.re.dslab.threatmodeling.type.entity.Control;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Builder
+@AllArgsConstructor
 public class ControlResponseDto {
 
     private String controlId;
 
     private String controlName;
-
-    public static List<ControlResponseDto> of(List<Control> controls) {
-        return controls.stream()
-                .map(ControlResponseDto::of)
-                .toList();
-    }
-
-    public static ControlResponseDto of(Control control) {
-        return ControlResponseDto.builder()
-                .controlId(control.getControlId())
-                .controlName(control.getControlName())
-                .build();
-    }
 
 }
