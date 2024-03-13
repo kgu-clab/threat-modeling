77e476faf9de5c444a8da0dfcf97ba91b65cc0bd
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-13 23:19:51 +0900

    feat: add query client (#15)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.tsx';
+import { Toaster } from 'react-hot-toast';
 import '@styles/globals.css';
 import '@utils/i18n.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@hooks/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
