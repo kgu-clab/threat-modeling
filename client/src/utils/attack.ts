b821fb6c7dd9471d8a6bb3daa31021e0b17e75b4
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-14 01:11:27 +0900

    refactor: 타입 이름 변경 (#20)

--- client/src/utils/attack.ts
+++ client/src/utils/attack.ts
@@ -1,6 +1,6 @@
 import type { AttackFlowJsonType } from '@type/file';
 
-export interface ParsedResult {
+export interface AttackFlowResultType {
   [techniqueId: string]: string[];
 }
 
@@ -10,11 +10,13 @@ export interface ParsedResult {
  * 이 함수는 `attack-action`의 `technique_id`를 키로 하고, 해당 `technique_id`에 연결된 모든 `attack-condition`의 `description`을 배열로 가지는 객체를 반환합니다.
  *
  * @param {AttackFlowJsonType} json - 공격 흐름 데이터를 포함하는 JSON 객체.
- * @returns {ParsedResult} 결과 객체, 여기서 `technique_id`별로 여러 `description`을 배열로 포함합니다.
+ * @returns {AttackFlowResultType} 결과 객체, 여기서 `technique_id`별로 여러 `description`을 배열로 포함합니다.
  */
 
-export const parserAttackFlow = (json: AttackFlowJsonType): ParsedResult => {
-  const result: ParsedResult = {};
+export const parserAttackFlow = (
+  json: AttackFlowJsonType,
+): AttackFlowResultType => {
+  const result: AttackFlowResultType = {};
   const attackConditionMap: { [key: string]: string } = {};
 
   json.objects
