2b918d6e24e41af851fd5914b142019a6c41ff44
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-15 21:12:07 +0900

    feat: api suspense 적용 (#22)

--- client/src/router/Layout.tsx
+++ client/src/router/Layout.tsx
@@ -1,17 +1,20 @@
 import Footer from '@/components/common/Footer/Footer';
 import ScrollToTop from '@/components/common/ScrollToTop/ScrollToTop';
 import Navbar from '@components/common/Navbar/Navbar';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <ScrollToTop>
-      <Navbar />
-      <main className="container">
-        <Outlet />
-      </main>
-      <Footer />
-    </ScrollToTop>
+    <Suspense>
+      <ScrollToTop>
+        <Navbar />
+        <main className="container">
+          <Outlet />
+        </main>
+        <Footer />
+      </ScrollToTop>
+    </Suspense>
   );
 };
 
