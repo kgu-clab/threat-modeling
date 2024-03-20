ff9352b939b6a873300e0e5a8d99a693a8ca763c
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-20 17:05:10 +0900

    refactor: add notion constant (#25)

--- client/src/pages/GuidePage/GuidePage.tsx
+++ client/src/pages/GuidePage/GuidePage.tsx
@@ -1,3 +1,4 @@
+import { NOTION } from '@constants/api';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BlockMapType, NotionRenderer } from 'react-notion';
@@ -7,12 +8,9 @@ const GuidePage = () => {
   const [data, setData] = useState<BlockMapType>({});
 
   useEffect(() => {
-    const NOTION_PAGE_ID = '94157834e2404a4c821b41ba518c4080';
-    fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+    fetch(`https://notion-api.splitbee.io/v1/page/${NOTION.GUIDE}`)
       .then((res) => res.json())
-      .then((resJson) => {
-        setData(resJson);
-      });
+      .then((data) => setData(data));
   }, []);
 
   return (
