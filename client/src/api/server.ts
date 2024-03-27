616a0faa478db6bfb1614d2d0ac472ac3d39bb59
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-27 14:49:58 +0900

    refactor: baseURL 개발환경과 프로덕션 환경에 따라 설정하도록 변경 (#34)

--- client/src/api/server.ts
+++ client/src/api/server.ts
@@ -1,8 +1,16 @@
 import { API_BASE_URL } from '@constants/api';
+import { ENVIRONMENT_MODE } from '@constants/common';
 import ServerChain from '@gwansikk/server-chain';
 
 export const server = ServerChain({
   key: 'tm',
+  /**
+   * development mode는 http 통신도 허용합니다.
+   */
   mode: 'development',
-  baseURL: API_BASE_URL,
+  /**
+   * API 서버의 기본 URL을 설정합니다.
+   * 개발환경일 경우 서버 URL를 사용하고, 프로덕션 환경일 경우 프록시 URL을 사용합니다.
+   */
+  baseURL: ENVIRONMENT_MODE === 'development' ? API_BASE_URL : '/api/v1',
 });
